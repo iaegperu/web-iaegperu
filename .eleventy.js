9787cbd51b06f3023bba97fa96b333f7e455deb9
iaegperu <iaegperu@gmail.com>
2026-07-08 13:06:42 +0800

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,13 @@
 module.exports = function (eleventyConfig) {
-  // El CSS real que usan las páginas es "styles.css" (no "style.css")
+  // CSS
   eleventyConfig.addPassthroughCopy("src/styles.css");
 
-  // Copia todas las imágenes que estén sueltas dentro de src/
+  // Imágenes sueltas dentro de src/
   eleventyConfig.addPassthroughCopy("src/*.png");
   eleventyConfig.addPassthroughCopy("src/*.jpg");
   eleventyConfig.addPassthroughCopy("src/*.jpeg");
   eleventyConfig.addPassthroughCopy("src/*.svg");
   eleventyConfig.addPassthroughCopy("src/*.webp");
-
-  // Si organizás algunas imágenes dentro de src/img, descomenta esta línea:
-  // eleventyConfig.addPassthroughCopy("src/img");
 
   return {
     dir: {
